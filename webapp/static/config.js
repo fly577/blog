a86938e53e8b1edb25230cdd1a358a55b5153dbd
fly577 <2300342537@qq.com>
2026-07-15 15:22:44 +0800

    Point frontend to hzx PythonAnywhere backend

--- webapp/static/config.js
+++ webapp/static/config.js
@@ -1,4 +1,4 @@
 // Public frontend configuration. Do not put API keys in this file.
 // GitHub Pages hosts the frontend at https://fly577.github.io/blog/.
 // PythonAnywhere hosts the FastAPI backend.
-window.WEATHER_AGENT_API_BASE_URL = "https://fly577.pythonanywhere.com";
+window.WEATHER_AGENT_API_BASE_URL = "https://hzx.pythonanywhere.com";
